0d24014b82370075c96bac3434f6749ea3139633
Hugo Leonel <hugoleonel.code@gmail.com>
2023-06-19 04:01:35 -0300

    Leitura Arquivo - Try With Resources

---
+++ LeituraArquivo/src/com/aulao002/TryWithResources.java
@@ -0,0 +1,29 @@
+package com.aulao002;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TryWithResources {
+
+	public static void main(String[] args) {
+		
+String path = "src/planilha.csv";
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			
+			String line = br.readLine();
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+			
+		} catch(IOException e) {
+			
+			System.out.println("Error: " + e.getMessage());
+			
+		}
+		
+	}
+	
+}
